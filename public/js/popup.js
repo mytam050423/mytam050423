51dca8df971f15ddb50e0788962565aa1a01ac49
mytam050423 <mytam050423@gmail.com>
2025-12-14 20:19:11 +0700

    Add files via upload

---
+++ public/js/popup.js
@@ -0,0 +1,199 @@
+function openPopup1() {
+    document.getElementById("popup").style.display = "flex";
+}
+
+function closePopup1() {
+    document.getElementById("popup").style.display = "none";
+}
+
+function openPopup2() {
+    document.getElementById("popup2").style.display = "flex";
+}
+
+function closePopup2() {
+    document.getElementById("popup2").style.display = "none";
+}
+function openPopup3() {
+    document.getElementById("popup3").style.display = "flex";
+}
+
+function closePopup3() {
+    document.getElementById("popup3").style.display = "none";
+}
+function openPopup4() {
+    document.getElementById("popup4").style.display = "flex";
+}
+
+function closePopup4() {
+    document.getElementById("popup4").style.display = "none";
+}
+function openPopup5() {
+    document.getElementById("popup5").style.display = "flex";
+}
+
+function closePopup5() {
+    document.getElementById("popup5").style.display = "none";
+}
+function openPopup6() {
+    document.getElementById("popup6").style.display = "flex";
+}
+
+function closePopup6() {
+    document.getElementById("popup6").style.display = "none";
+}
+function openPopup7() {
+    document.getElementById("popup7").style.display = "flex";
+}
+
+function closePopup7() {
+    document.getElementById("popup7").style.display = "none";
+}
+function openPopup8() {
+    document.getElementById("popup8").style.display = "flex";
+}
+
+function closePopup8() {
+    document.getElementById("popup8").style.display = "none";
+}
+function openPopup9() {
+    document.getElementById("popup9").style.display = "flex";
+}
+
+function closePopup9() {
+    document.getElementById("popup9").style.display = "none";
+}
+function openPopup10() {
+    document.getElementById("popup10").style.display = "flex";
+}
+
+function closePopup10() {
+    document.getElementById("popup10").style.display = "none";
+}
+function openPopup11() {
+    document.getElementById("popup11").style.display = "flex";
+}
+
+function closePopup11() {
+    document.getElementById("popup11").style.display = "none";
+}
+function openPopup12() {
+    document.getElementById("popup12").style.display = "flex";
+}
+
+function closePopup12() {
+    document.getElementById("popup12").style.display = "none";
+}
+function openPopup13() {
+    document.getElementById("popup13").style.display = "flex";
+}
+
+function closePopup13() {
+    document.getElementById("popup13").style.display = "none";
+}
+function openPopup14() {
+    document.getElementById("popup14").style.display = "flex";
+}
+
+function closePopup14() {
+    document.getElementById("popup14").style.display = "none";
+}
+function openPopup15() {
+    document.getElementById("popup15").style.display = "flex";
+}
+
+function closePopup15() {
+    document.getElementById("popup15").style.display = "none";
+}
+function openPopup16() {
+    document.getElementById("popup16").style.display = "flex";
+}
+
+function closePopup16() {
+    document.getElementById("popup16").style.display = "none";
+}
+function openPopup17() {
+    document.getElementById("popup17").style.display = "flex";
+}
+
+function closePopup17() {
+    document.getElementById("popup17").style.display = "none";
+}
+function openPopup18() {
+    document.getElementById("popup18").style.display = "flex";
+}
+
+function closePopup18() {
+    document.getElementById("popup18").style.display = "none";
+}
+function openPopup19() {
+    document.getElementById("popup19").style.display = "flex";
+}
+
+function closePopup19() {
+    document.getElementById("popup19").style.display = "none";
+}
+function openPopup20() {
+    document.getElementById("popup20").style.display = "flex";
+}
+
+function closePopup20() {
+    document.getElementById("popup20").style.display = "none";
+}
+
+function openPopup21() {
+    document.getElementById("popup21").style.display = "flex";
+}
+
+function closePopup21() {
+    document.getElementById("popup21").style.display = "none";
+}
+function openPopup22() {
+    document.getElementById("popup22").style.display = "flex";
+}
+
+function closePopup22() {
+    document.getElementById("popup22").style.display = "none";
+}
+function openPopup23() {
+    document.getElementById("popup23").style.display = "flex";
+}
+
+function closePopup23() {
+    document.getElementById("popup23").style.display = "none";
+}
+function openPopup24() {
+    document.getElementById("popup24").style.display = "flex";
+}
+
+function closePopup24() {
+    document.getElementById("popup24").style.display = "none";
+}
+function openPopup25() {
+    document.getElementById("popup25").style.display = "flex";
+}
+
+function closePopup25() {
+    document.getElementById("popup25").style.display = "none";
+}
+function openPopup26() {
+    document.getElementById("popup26").style.display = "flex";
+}
+
+function closePopup26() {
+    document.getElementById("popup26").style.display = "none";
+}
+function openPopup27() {
+    document.getElementById("popup27").style.display = "flex";
+}
+
+function closePopup27() {
+    document.getElementById("popup27").style.display = "none";
+}
+function openPopup28() {
+    document.getElementById("popup28").style.display = "flex";
+}
+
+function closePopup28() {
+    document.getElementById("popup28").style.display = "none";
+}
+
